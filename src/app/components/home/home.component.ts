c5ea4b09ca615e78ece47dd3de2ee30ac86dc5ac
MK-CIAN <cian.mck20@gmail.com>
2024-01-31 16:42:11 +0000

    Games API Working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from '../../models';
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
     public sort: string = '';
-    constructor() { }
+    public games: Array<Game> = [];
+
+    constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
+      this.activatedRoute.params.subscribe((params: Params) => {
+        if (params['game-search']) {
+          this.searchGames('metacrit', params['game-search']);
+        } else {
+          this.searchGames('metacrit');
+        }
+      });
     }
 
-
+    searchGames(sort: string, search?: string): void { // Fix the parameter name
+      this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+    });
+  }
 }
--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -11,12 +11,12 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders: {
-                'X-RapidAPI-Key': 'f50b86853emshe5b1e2ce9401e8ep185df6jsn4ea1c2b93c15',
-                'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
-            },
-            setParams: {
-                key: 'f50b86853emshe5b1e2ce9401e8ep185df6jsn4ea1c2b93c15'
-            }
+                'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+              },
+              setParams: {
+                key: 'e40e743af2c94b0c916a8aa618fb4473',
+              }
         });
         return next.handle(req);
     }
