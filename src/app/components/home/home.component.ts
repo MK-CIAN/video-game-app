a283c1980b4556863536e1151d3e2a5f64d1d507
MK-CIAN <cian.mck20@gmail.com>
2024-02-01 12:38:49 +0000

    Game ID Working, Problems with further retrival of information

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,41 +1,44 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HttpService } from '../../services/http.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { APIResponse, Game } from '../../models';
 import { Subscription } from 'rxjs';
-
+import { APIResponse, Game } from '../../models';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy{
-    public sort: string = '';
-    public games: Array<Game> = [];
-    private routeSub: Subscription = new Subscription();
-    private gameSub: Subscription = new Subscription();
-    constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, private router:Router) { }
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string = '';
+  public games: Array<Game> = [];
+  private routeSub: Subscription  = new Subscription();
+  private gameSub: Subscription = new Subscription();
 
-    ngOnInit(): void {
-        this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
-        if (params['game-search']) {
-          this.searchGames('metacrit', params['game-search']);
-        } else {
-          this.searchGames('metacrit');
-        }
-      });
-    }
+  constructor(    
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
-    searchGames(sort: string, search?: string): void { // Fix the parameter name
-      this.gameSub = this.httpService
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList);
-    });
+      });
   }
-    
+
   openGameDetails(id: string): void {
     this.router.navigate(['details', id]);
   }
@@ -49,5 +52,5 @@ export class HomeComponent implements OnInit, OnDestroy{
       this.routeSub.unsubscribe();
     }
   }
-}
 
+}
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,4 +1,5 @@
 export interface Game {
+    id: string;
     background_image: string;
     name: string;
     released: string;
@@ -8,43 +9,44 @@ export interface Game {
     metacritic: number;
     genres: Array<Genre>;
     parent_platforms: Array<ParentPlatform>;
-    publishers: Array<Publisher>;
+    publishers: Array<Publishers>;
     ratings: Array<Rating>;
     screenshots: Array<Screenshots>;
     trailers: Array<Trailer>;
-}
-
-export interface APIResponse<T> {
-    results: Array<T>;
-}
-
-interface Genre {
+  }
+  
+  export interface APIResponse<T> {
+      results: Array<T>;
+  }
+  
+  interface Genre {
     name: string;
-}
-
-interface ParentPlatform {
+  }
+  
+  interface ParentPlatform {
     platform: {
-    slug: string;
-    name: string;
+      name: string;
+      slug: string;
     };
-}
-
-interface Publisher {
+  }
+  
+  interface Publishers {
     name: string;
-}
-
-interface Rating {
+  }
+  
+  interface Rating {
     id: number;
     count: number;
     title: string;
-}
-
-interface Screenshots {
+  }
+  
+  interface Screenshots {
     image: string;
-}
-
-interface Trailer {
+  }
+  
+  interface Trailer {
     data: {
-        max: string;
+      max: string;
     };
-}
+  }
+  
